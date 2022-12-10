b59a8d2c4bcea654aa437a73d7a4972bc5210750
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2022-12-10 13:25:46 +0300

    commit tree ce68582752e1708616c826009dab6c0bb126c929

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (word === word.toLowerCase().split("").reverse().join("")) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  function isPalindrome(word) {
+  // Convert the word to lowercase
+  word = word.toLowerCase()
+  
+  // Split the word into an array of individual characters
+  word_chars = word.split("")
+  
+  // Reverse the order of the characters in the array
+  word_chars = word_chars.reverse()
+  
+  // Join the characters back together into a single string
+  word = word_chars.join("")
+  
+  // Check if the resulting string is the same as the original word
+  if (word === word.toLowerCase().split("").reverse().join("")) {
+    // If it is, return true
+    return true
+  } else {
+    // If it is not, return false
+    return false
+  }
+}
+
 */
 
 /*
   Add written explanation of your solution here
+
+  This function checks if a given word is a palindrome, 
+  which means that it is the same word when read forwards and backwards.
+  It does this by first converting the word to lowercase, 
+  then splitting it into an array of individual characters,
+  reversing the order of the characters in the array,
+  and finally joining the characters back together into a single string.
+  If the resulting string is the same as the original word, 
+  then the function returns true, otherwise it returns false.
+  
 */
 
 // You can run `node index.js` to view these console logs
